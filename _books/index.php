50cdbb53b7804d6848655685eb83acee0aabd793
Mateus-Romera <mr-villar@hotmail.com>
2017-06-16 14:05:53 -0300

    Teste

--- _books/index.php
+++ _books/index.php
@@ -1,10 +1,6 @@
-<!--PÁGINA PRINCIPAL-->
+﻿<!--PÁGINA PRINCIPAL-->
 <?php
-// conecta ao banco de dados
-$connect = mysqli_connect('localhost','root','');
-
-// seleciona a base de dados em que vamos trabalhar
-$db = mysqli_select_db($connect, 'sandvigbookstore');
+include_once('DatabaseConnection.php');
 
 // cria a instrução SQL que vai selecionar os dados
 $query_select = "SELECT * FROM bookcategories";
