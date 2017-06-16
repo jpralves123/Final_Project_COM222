b892285b2e061551dd4fd091943c9f248426fc4e
jpralves123 <jpralves_unifei@hotmail.com>
2017-06-16 15:33:17 -0300

    footer and header

--- _books/BookStoreManagement.php
+++ _books/BookStoreManagement.php
@@ -22,33 +22,9 @@ $total = mysqli_num_rows($select);
 
     <title>WB - Store Management</title>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="style.css">
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-    <nav class="navbar ">
-      <div class="container-fluid">
-
-        <div class="navbar-header">
-          <a class="navbar-brand" href="index.php"><img class="logo" src="img/logo.png"></a>
-        </div>
-
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-          <li><a href="login_page.html"><span class="glyphicon glyphicon-log-out"></span> Exit</a></li>
-        </ul>
-
-      </div>
-    </nav>
+    <?php
+    include_once('header.php');
+    ?>
 
   </head>
 
