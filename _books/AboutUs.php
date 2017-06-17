64f2f8131ec978e88c61797a4049d22a4ce5e82d
Mateus-Romera <mr-villar@hotmail.com>
2017-06-17 13:21:09 -0300

    ChangeAboutUs

--- _books/AboutUs.php
+++ _books/AboutUs.php
@@ -20,10 +20,17 @@
   <body>
 
     <div class="container-fluid col-md-10">
-      <h3>About this website...</h3>
+      <h3><b>About this website...</b></h3>
 
       <p>
-        This website is a project developed in the discipline of Web Programming, the goal of this project is to implement a Online Book Store, using HTML, CSS, PHP and others consepts.
+        This website is a project developed in the discipline of Web Programming, the goal of this project is to implement an Online Book Store, using HTML, CSS, PHP and others concepts.
+      </p>
+
+      <br>
+
+      <p>
+        <h4><b>Some other comments regarding this website:</b></h4>
+	<br>It is important to note that the only way to create a user admin is by altering the column "admin" on the database.
       </p>
 
 
