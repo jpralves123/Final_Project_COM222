bf94852fd40b9834d82882f384ad25a418ac5e61
Mateus-Romera <mr-villar@hotmail.com>
2017-06-17 23:34:06 -0300

    IndexRandom

---
+++ _books/RandomBookFunctions.php
@@ -0,0 +1,59 @@
+﻿<?php
+
+	function randomBook() {
+		// Conecta ao banco de dados e seleciona a base de dados em que vamos trabalhar
+		include('DatabaseConnection.php');
+
+		// cria a instrução SQL que vai selecionar os dados
+		$query_select = "SELECT * FROM bookdescriptions ORDER BY RAND() LIMIT 76";
+
+		// executa a query
+		$select = mysqli_query($connect, $query_select);
+
+		// transforma os dados em um array
+		$row = mysqli_fetch_assoc($select);
+
+		// calcula quantos dados retornaram
+		$total = mysqli_num_rows($select);
+
+		$row = mysqli_fetch_assoc($select);
+
+		return $row;
+	}
+
+	function trimString($FullString){
+		$string = strip_tags($FullString);
+
+		if (strlen($string) > 50) {
+
+			// truncate string
+			$stringCut = substr($string, 0, 500);
+
+			// make sure it ends in a word so assassinate doesn't become ass...
+ 			$string = substr($stringCut, 0, strrpos($stringCut, ' ')); 
+
+		}
+
+		return $string;
+	}
+
+	function randomBookShow(){
+		echo "<li class=\"col-sm-4\">
+    			<div class=\"fff\">";
+				$rdmBook = randomBook();
+				echo "<div class=\"panel-heading\">
+                  			<h4 class=\"text-left\">
+                              			<a>".$rdmBook['title']."</a>
+                        		</h4>
+                  		</div>
+			<div class=\"col-md-4 img-responsive center-block\">
+				<img src=\"https://baldochi.unifei.edu.br/COM222/trabfinal/imagens/".$rdmBook['ISBN'].".01.THUMBZZZ.jpg\">
+			</div>
+			<div class=\"caption text-justify\">
+				<p>".trimString($rdmBook['description'])."</p>
+				<br><a href=\"ProductPage.php?ISBN=".$rdmBook['ISBN']."\">» Read More</a>
+			</div>
+			</div>
+		</li>";
+	}
+?>
--- _books/index.php
+++ _books/index.php
@@ -1,10 +1,6 @@
 ﻿<!--PÁGINA PRINCIPAL-->
 
-<?php
-// Conecta ao banco de dados e seleciona a base de dados em que vamos trabalhar
-include_once('DatabaseConnection.php');
 
-?>
 
 <!DOCTYPE hml>
 <html lang="en">
@@ -20,6 +16,8 @@ include_once('DatabaseConnection.php');
     <!-- ************************************************ -->
     <!--SIDE MENU-->
     <?php  include 'menus/sideMenu.php'; ?>
+    <!--RANDOM BOOKS FUNCTIONS-->
+    <?php  include 'RandomBookFunctions.php'; ?>
     <!-- ************************************************ -->
 
 
@@ -28,8 +26,6 @@ include_once('DatabaseConnection.php');
 <!-- ************************************************ -->
 
   <body>
-
-
 
     <!--SLIDER RANDON BOOKS-->
 
@@ -45,37 +41,27 @@ include_once('DatabaseConnection.php');
             <div class="col-md-10">
               <div class="panel">
                 <div class="panel-body">
+		 <?php 
+		   randomBookShow();
+                   randomBookShow();
+		   randomBookShow();
+		 ?>
+                </div>
+		</div>";
+            </div>
+          </div>
 
-                <?php
-                      // cria a instrução SQL que vai selecionar os dados
-                      $query_select = "SELECT * FROM bookdescriptions ORDER BY RAND() LIMIT 76";
-
-                      // executa a query
-                      $select = mysqli_query($connect, $query_select);
-
-                      // transforma os dados em um array
-                      $row = mysqli_fetch_assoc($select);
-
-                      // calcula quantos dados retornaram
-                      $total = mysqli_num_rows($select);
-
-                      $row = mysqli_fetch_assoc($select);
-
-                      echo "<div class=\"panel-heading\">
-                              <h4 class=\"text-left\">
-                                <a>
-                                  ".$row['title']."
-                                </a>
-                              </h4>
-                            </div>
-                            <img class=\"col-md-2 img-responsive center-block\" src=\"https://baldochi.unifei.edu.br/COM222/trabfinal/imagens/".$row['ISBN'].".01.THUMBZZZ.jpg\">
-
-                      <div class=\"col-md-10 text-justify\">
-                        ".$row['description']."
-                        <a>Read More</a>
-                      </div>";
-
-                  ?>
+          <div class="item">
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-10">
+              <div class="panel">
+                <div class="panel-body">
+                <?php 
+		   randomBookShow();
+                   randomBookShow();
+		   randomBookShow();
+		 ?>
                 </div>
               </div>
             </div>
@@ -87,80 +73,11 @@ include_once('DatabaseConnection.php');
             <div class="col-md-10">
               <div class="panel">
                 <div class="panel-body">
-
-                <?php
-
-                      // cria a instrução SQL que vai selecionar os dados
-                      $query_select = "SELECT * FROM bookdescriptions ORDER BY RAND() LIMIT 76";
-
-                      // executa a query
-                      $select = mysqli_query($connect, $query_select);
-
-                      // transforma os dados em um array
-                      $row = mysqli_fetch_assoc($select);
-
-                      // calcula quantos dados retornaram
-                      $total = mysqli_num_rows($select);
-
-                      $row = mysqli_fetch_assoc($select);
-
-                      echo "<div class=\"panel-heading\">
-                              <h4 class=\"text-left\">
-                                <a>
-                                  ".$row['title']."
-                                </a>
-                              </h4>
-                            </div>
-                            <img class=\"col-md-2 img-responsive center-block\" src=\"https://baldochi.unifei.edu.br/COM222/trabfinal/imagens/".$row['ISBN'].".01.THUMBZZZ.jpg\">
-
-                            <div class=\"col-md-10 text-justify\">
-                              ".$row['description']."
-                              <a>Read More</a>
-                            </div>";
-
-                  ?>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="item">
-            <div class="col-md-1">
-            </div>
-            <div class="col-md-10">
-              <div class="panel">
-                <div class="panel-body">
-
-                <?php
-
-                      // cria a instrução SQL que vai selecionar os dados
-                      $query_select = "SELECT * FROM bookdescriptions ORDER BY RAND() LIMIT 76";
-
-                      // executa a query
-                      $select = mysqli_query($connect, $query_select);
-
-                      // transforma os dados em um array
-                      $row = mysqli_fetch_assoc($select);
-
-                      // calcula quantos dados retornaram
-                      $total = mysqli_num_rows($select);
-
-                      $row = mysqli_fetch_assoc($select);
-
-                      echo "<div class=\"panel-heading\">
-                              <h4 class=\"text-left\">
-                                <a>
-                                  ".$row['title']."
-                                </a>
-                              </h4>
-                            </div>
-                            <img class=\"col-md-2 img-responsive center-block\" src=\"https://baldochi.unifei.edu.br/COM222/trabfinal/imagens/".$row['ISBN'].".01.THUMBZZZ.jpg\">
-
-                            <div class=\"col-md-10 text-justify\">
-                              ".$row['description']."
-                              <a>Read More</a>
-                            </div>";
-                  ?>
+               <?php 
+		   randomBookShow();
+                   randomBookShow();
+		   randomBookShow();
+		 ?>
                 </div>
               </div>
             </div>
